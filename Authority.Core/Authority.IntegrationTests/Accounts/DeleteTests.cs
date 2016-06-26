fc4d4123e18563da86a01ed9ca87d5e6f4574f6a
reiteristvan <reiteristvan@gmail.com>
2016-06-26 14:44:08 +0200

    Delete user operation + test

---
+++ Authority.Core/Authority.IntegrationTests/Accounts/DeleteTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authority.DomainModel;
+using Authority.IntegrationTests.Common;
+using Authority.IntegrationTests.Fixtures;
+using Authority.Operations.Account;
+using Xunit;
+
+namespace Authority.IntegrationTests.Accounts
+{
+    public sealed class DeleteTests : IClassFixture<AccountTestFixture>
+    {
+        private readonly AccountTestFixture _fixture;
+
+        public DeleteTests(AccountTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task DeleteUserShouldSucceed()
+        {
+            User user = await TestOperations.RegisterAndActivateUser(
+                _fixture.Context, 
+                _fixture.Domain.Id,
+                RandomData.RandomString());
+
+            DeleteUser operation = new DeleteUser(_fixture.Context, _fixture.Domain.Id, user.Email);
+            await operation.Do();
+            await operation.CommitAsync();
+
+            Guid domainId = _fixture.Domain.Id;
+            string email = user.Email;
+
+            user = await _fixture.Context.Users
+                .FirstOrDefaultAsync(u => u.DomainId == domainId && u.Email == email);
+
+            Assert.Null(user);
+        }
+    }
+}
---
+++ Authority.Core/Authority.Operations/Account/DeleteUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Authority.DomainModel;
+using Authority.EntityFramework;
+
+namespace Authority.Operations.Account
+{
+    public sealed class DeleteUser : OperationWithNoReturnAsync
+    {
+        private readonly Guid _domainId;
+        private readonly string _email;
+
+        public DeleteUser(IAuthorityContext authorityContext, Guid domainId, string email) 
+            : base(authorityContext)
+        {
+            _domainId = domainId;
+            _email = email;
+        }
+
+        public override async Task Do()
+        {
+            User user = await Context.Users.FirstOrDefaultAsync(u => u.DomainId == _domainId && u.Email == _email);
+
+            if (user == null)
+            {
+                throw new RequirementFailedException(AccountErrorCodes.UserNotFound);
+            }
+
+            Context.Users.Remove(user);
+        }
+    }
+}
