d3bbb983067936ac63ee3cfe5ce9f454879fcb11
reiteristvan <reiteristvan@gmail.com>
2016-07-09 13:40:08 +0200

    Delete domain takes care of existing users too

--- Authority.Core/Authority.IntegrationTests/Domains/DeleteDomainTests.cs
+++ Authority.Core/Authority.IntegrationTests/Domains/DeleteDomainTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 using Authority.DomainModel;
+using Authority.IntegrationTests.Common;
 using Authority.IntegrationTests.Fixtures;
 using Authority.Operations.Products;
 using Xunit;
@@ -30,6 +33,33 @@ namespace Authority.IntegrationTests.Domains
             Domain domain = await _fixture.Context.Domains.FirstOrDefaultAsync(d => d.Id == domainId);
 
             Assert.Null(domain);
+        }
+
+        [Fact]
+        public async Task DeleteDomainWithUsersShouldSucceed()
+        {
+            CreateDomain createOperation = new CreateDomain(_fixture.Context, RandomData.RandomString());
+            Guid domainId = await createOperation.Do();
+            await createOperation.CommitAsync();
+
+            // TODO: bulk register
+            for (int i = 0; i < 100; ++i)
+            {
+                await TestOperations.RegisterAndActivateUser(_fixture.Context, domainId, RandomData.RandomString(12, true));
+            }
+
+            DeleteDomain deleteOperation = new DeleteDomain(_fixture.Context, domainId);
+            await deleteOperation.Execute();
+
+            Domain domain = await _fixture.Context.Domains.FirstOrDefaultAsync(d => d.Id == domainId);
+
+            Assert.Null(domain);
+
+            List<User> users = _fixture.Context.Users
+                .Where(u => u.DomainId == domainId)
+                .ToList();
+
+            Assert.False(users.Any());
         }
     }
 }
--- Authority.Core/Authority.Operations/Domains/DeleteDomain.cs
+++ Authority.Core/Authority.Operations/Domains/DeleteDomain.cs
@@ -28,6 +28,10 @@ namespace Authority.Operations.Products
             }
 
             await Context.Database.ExecuteSqlCommandAsync(
+                "delete from Authority.Users where DomainId = @DomainId",
+                new SqlParameter("@DomainId", _domainId));
+
+            await Context.Database.ExecuteSqlCommandAsync(
                 "delete from Authority.Domains where Id = @Id",
                 new SqlParameter("@Id", _domainId));
         }
