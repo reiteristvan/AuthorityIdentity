1adda7f0f251bffd0aeb2c233b8977d87fe2257b
reiteristvan <reiteristvan@gmail.com>
2016-08-02 19:53:50 +0200

    Add Claims to Policy operation in progress

---
+++ Authority.Core/Authority.Operations/Policies/AddClaimsToPolicy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authority.DomainModel;
+using Authority.EntityFramework;
+
+namespace Authority.Operations.Policies
+{
+    public sealed class AddClaimsToPolicy : OperationWithNoReturnAsync
+    {
+        private readonly Guid _policyId;
+        private readonly IEnumerable<Guid> _claims;
+
+        public AddClaimsToPolicy(IAuthorityContext authorityContext, Guid policyId, IEnumerable<Guid> claims) 
+            : base(authorityContext)
+        {
+            _policyId = policyId;
+            _claims = claims;
+        }
+
+        public override async Task Do()
+        {
+            Policy policy = await Context.Policies.FirstOrDefaultAsync(p => p.Id == _policyId);
+
+            Require(() => policy != null, PolicyErrorCodes.PolicyNotFound);
+        }
+    }
+}
