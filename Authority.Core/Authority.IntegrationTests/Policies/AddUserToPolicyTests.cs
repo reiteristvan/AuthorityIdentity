2f56116c9c30ce2f79204863cbf79d5f0d419f17
Istvan Reiter <reiteristvan@gmail.com>
2016-08-04 14:35:43 +0200

    Add user to policy done

---
+++ Authority.Core/Authority.IntegrationTests/Policies/AddUserToPolicyTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Authority.DomainModel;
+using Authority.IntegrationTests.Common;
+using Authority.Operations.Policies;
+using Xunit;
+
+namespace Authority.IntegrationTests.Policies
+{
+    public sealed class AddUserToPolicyTests
+    {
+        [Fact]
+        public async Task AddUserToPolicyShouldSucceed()
+        {
+            using (AuthorityTestContext testContext = new AuthorityTestContext())
+            {
+                // Arrange
+                User user = await TestOperations.RegisterAndActivateUser(testContext.Context, testContext.Domain.Id, RandomData.RandomString());
+                Policy policy = await TestOperations.CreatePolicy(testContext.Context, testContext.Domain.Id, RandomData.RandomString(), false);
+
+                // Act
+                AddUserToPolicy addToPolicy = new AddUserToPolicy(testContext.Context, user.Id, policy.Id);
+                await addToPolicy.Do();
+                await addToPolicy.CommitAsync();
+
+                // Assert
+                Guid userId = user.Id;
+                user = await testContext.Context.Users
+                    .Include(u => u.Policies)
+                    .FirstOrDefaultAsync(u => u.Id == userId);
+
+                Assert.NotEmpty(user.Policies);
+                Assert.True(user.Policies.First().Name == policy.Name);
+            }
+        }
+    }
+}
