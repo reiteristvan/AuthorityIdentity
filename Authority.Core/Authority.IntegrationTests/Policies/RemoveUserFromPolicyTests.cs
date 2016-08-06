480b8b58b44bbc5832eb17f0ed1ceeb93ecd1019
reiteristvan <reiteristvan@gmail.com>
2016-08-06 14:01:49 +0200

    Remove user from policy: operation + test

---
+++ Authority.Core/Authority.IntegrationTests/Policies/RemoveUserFromPolicyTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Authority.DomainModel;
+using Authority.IntegrationTests.Common;
+using Authority.Operations.Policies;
+using Xunit;
+
+namespace Authority.IntegrationTests.Policies
+{
+    public sealed class RemoveUserFromPolicyTests
+    {
+        [Fact]
+        public async Task RemoveUserFromPolicyShouldSucceed()
+        {
+            using (AuthorityTestContext testContext = new AuthorityTestContext())
+            {
+                // Arrange
+                User user = await TestOperations.RegisterAndActivateUser(testContext.Context, testContext.Domain.Id, RandomData.RandomString());
+                Policy policy = await TestOperations.CreatePolicy(testContext.Context, testContext.Domain.Id, RandomData.RandomString());
+                
+                AddUserToPolicy addToPolicy = new AddUserToPolicy(testContext.Context, user.Id, policy.Id);
+                await addToPolicy.Do();
+                await addToPolicy.CommitAsync();
+
+                // Act
+                RemoveUserFromPolicy removeFromPolicy = new RemoveUserFromPolicy(testContext.Context, user.Id, policy.Id);
+                await removeFromPolicy.Do();
+                await removeFromPolicy.CommitAsync();
+
+                // Assert
+                Guid userId = user.Id;
+                user = await testContext.Context.Users
+                    .Include(u => u.Policies)
+                    .FirstOrDefaultAsync(u => u.Id == userId);
+
+                Assert.Empty(user.Policies);
+            }
+        }
+    }
+}
---
+++ Authority.Core/Authority.Operations/Policies/RemoveUserFromPolicy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Authority.DomainModel;
+using Authority.EntityFramework;
+
+namespace Authority.Operations.Policies
+{
+    public sealed class RemoveUserFromPolicy : OperationWithNoReturnAsync
+    {
+        private readonly Guid _userId;
+        private readonly Guid _policyId;
+
+        public RemoveUserFromPolicy(IAuthorityContext authorityContext, Guid userId, Guid policyId) 
+            : base(authorityContext)
+        {
+            _userId = userId;
+            _policyId = policyId;
+        }
+
+        public override async Task Do()
+        {
+            User user = await Context.Users
+                .Include(u => u.Policies)
+                .FirstOrDefaultAsync(u => u.Id == _userId);
+
+            Require(() => user != null, PolicyErrorCodes.UnAuthorizedAccess);
+
+            Policy policy = await Context.Policies.FirstOrDefaultAsync(p => p.Id == _policyId);
+
+            Require(() => policy != null, PolicyErrorCodes.PolicyNotFound);
+            Require(() => policy.DomainId == user.DomainId, PolicyErrorCodes.UnAuthorizedAccess);
+
+            user.Policies.Remove(policy);
+        }
+    }
+}
