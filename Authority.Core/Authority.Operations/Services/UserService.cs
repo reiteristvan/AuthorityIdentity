3c28aeb8080eb933e3a700871f025c74de8aae7e
reiteristvan <reiteristvan@gmail.com>
2016-08-12 16:11:47 +0200

    Added more service function for User services

--- Authority.Core/Authority.Operations/Services/UserService.cs
+++ Authority.Core/Authority.Operations/Services/UserService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using Authority.DomainModel;
 using Authority.EntityFramework;
 using Authority.Operations.Account;
+using Authority.Operations.Policies;
 
 namespace Authority.Operations.Services
 {
@@ -16,6 +18,12 @@ namespace Authority.Operations.Services
         Task Acivate(Guid activationCode);
         Task<LoginResult> Login(string email, string password, Guid domainId = new Guid());
         Task Delete(Guid userId);
+        Task SetStatus(Guid userId, bool isActive);
+        Task AddToPolicy(Guid userId, Guid policyId);
+        Task RemoveFromPolicy(Guid userId, Guid policyId);
+        Task BulkRegistration(List<BulkRegistrationData> registrationData, bool shouldActivate = false);
+        Guid Invite(string email, DateTimeOffset? expireOn = null, Guid domainId = new Guid());
+        Task FinalizeInvitation(Guid invitationCode, string username, string password);
     }
 
     public sealed class UserService : IUserService
@@ -108,11 +116,66 @@ namespace Authority.Operations.Services
             await deleteOperation.CommitAsync();
         }
 
-        public async Task SetStatus(Guid id)
+        public async Task SetStatus(Guid userId, bool isActive)
         {
-            
+            IAuthorityContext context = AuthorityContextProvider.Create();
+            SetUserStatus setStatus = new SetUserStatus(context, userId, isActive);
+            await setStatus.Do();
+            await setStatus.CommitAsync();
         }
 
+        public async Task AddToPolicy(Guid userId, Guid policyId)
+        {
+            IAuthorityContext context = AuthorityContextProvider.Create();
+            AddUserToPolicy addToPolicy = new AddUserToPolicy(context, userId, policyId);
+            await addToPolicy.Do();
+            await addToPolicy.CommitAsync();
+        }
+
+        public async Task RemoveFromPolicy(Guid userId, Guid policyId)
+        {
+            IAuthorityContext context = AuthorityContextProvider.Create();
+            RemoveUserFromPolicy removeFromPolicy = new RemoveUserFromPolicy(context, userId, policyId);
+            await removeFromPolicy.Do();
+            await removeFromPolicy.CommitAsync();
+        }
+
+        public async Task BulkRegistration(List<BulkRegistrationData> registrationData, bool shouldActivate = false)
+        {
+            IAuthorityContext context = AuthorityContextProvider.Create();
+            BulkUserRegistration bulkRegistration = new BulkUserRegistration(context, registrationData, shouldActivate);
+            await bulkRegistration.Execute();
+        }
+
+        public Guid Invite(string email, DateTimeOffset? expireOn = null, Guid domainId = new Guid())
+        {
+            if (domainId == Guid.Empty)
+            {
+                domainId = GetDomainId();
+            }
+
+            IAuthorityContext context = AuthorityContextProvider.Create();
+            InviteUser invite = new InviteUser(context, email, domainId, expireOn);
+            Guid invitationCode =  invite.Do();
+            invite.Commit();
+
+            return invitationCode;
+        }
+
+        public async Task FinalizeInvitation(Guid invitationCode, string username, string password)
+        {
+            IAuthorityContext context = AuthorityContextProvider.Create();
+            FinalizeInvitation finalizeInvitation = new FinalizeInvitation(context, invitationCode, username, password);
+            await finalizeInvitation.Do();
+            await finalizeInvitation.CommitAsync();
+        }
+
+        /// <summary>
+        /// This function retrieves the FIRST domain in the list if there was no domain id provided to the function
+        /// It can be used in single-domain scenarios
+        /// Do not use it in multi-domain scenarios
+        /// </summary>
+        /// <returns>The id of the first domain in the list</returns>
         private Guid GetDomainId()
         {
             if (!Authority.Domains.All().Any())
