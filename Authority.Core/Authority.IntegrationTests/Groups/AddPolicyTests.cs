70d68bcfb965892cafdb56487daafa327be81649
reiteristvan <reiteristvan@gmail.com>
2016-08-14 11:02:01 +0200

    Add policy to group operation and test

---
+++ Authority.Core/Authority.IntegrationTests/Groups/AddPolicyTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Authority.DomainModel;
+using Authority.IntegrationTests.Common;
+using Authority.Operations.Groups;
+using Xunit;
+
+namespace Authority.IntegrationTests.Groups
+{
+    public sealed class AddPolicyTests
+    {
+        [Fact]
+        public async Task AddPolicyToGroupShouldSucceed()
+        {
+            using (AuthorityTestContext testContext = new AuthorityTestContext())
+            {
+                // Arrange
+                Group group = await TestOperations.CreateGroup(testContext.Context, testContext.Domain.Id, RandomData.RandomString());
+                Policy policy = await TestOperations.CreatePolicy(testContext.Context, testContext.Domain.Id, RandomData.RandomString());
+
+                // Act
+                AddPolicyToGroup addToGroup = new AddPolicyToGroup(testContext.Context, group.Id, policy.Id);
+                await addToGroup.Do();
+                await addToGroup.CommitAsync();
+
+                // Assert
+                Guid groupId = group.Id;
+                Guid policyId = policy.Id;
+
+                group = await testContext.Context.Groups
+                    .Include(g => g.Policies)
+                    .FirstOrDefaultAsync(g => g.Id == groupId);
+
+                policy = await testContext.Context.Policies
+                    .Include(p => p.Groups)
+                    .FirstOrDefaultAsync(p => p.Id == policyId);
+
+                Assert.NotNull(group);
+                Assert.NotNull(policy);
+
+                Assert.Equal(1, group.Policies.Count);
+                Assert.Equal(1, policy.Groups.Count);
+
+                Assert.True(group.Policies.First().Id == policyId);
+                Assert.True(policy.Groups.First().Id == groupId);
+            }
+        }
+    }
+}
---
+++ Authority.Core/Authority.Operations/Groups/AddPolicyToGroup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Authority.DomainModel;
+using Authority.EntityFramework;
+
+namespace Authority.Operations.Groups
+{
+    public sealed class AddPolicyToGroup : OperationWithNoReturnAsync
+    {
+        private readonly Guid _groupId;
+        private readonly Guid _policyId;
+
+        public AddPolicyToGroup(IAuthorityContext authorityContext, Guid groupId, Guid policyId) 
+            : base(authorityContext)
+        {
+            _groupId = groupId;
+            _policyId = policyId;
+        }
+
+        public override async Task Do()
+        {
+            Group group = await Context.Groups
+                .Include(g => g.Policies)
+                .FirstOrDefaultAsync(g => g.Id == _groupId);
+
+            Require(() => group != null, ErrorCodes.GroupNotFound);
+
+            Policy policy = await Context.Policies.FirstOrDefaultAsync(p => p.Id == _policyId);
+
+            Require(() => policy != null, ErrorCodes.PolicyNotFound);
+            Require(() => policy.DomainId == group.DomainId, ErrorCodes.DomainMismatch);
+
+            if (group.Policies.Any(p => p.Id == _policyId))
+            {
+                return;
+            }
+
+            group.Policies.Add(policy);
+        }
+    }
+}
