c0eb7ab2948033453832610b0904362d93248296
reiteristvan <reiteristvan@gmail.com>
2016-08-14 15:13:25 +0200

    Remove policy from group operation and test

---
+++ Authority.Core/Authority.IntegrationTests/Groups/RemovePolicyTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Authority.DomainModel;
+using Authority.IntegrationTests.Common;
+using Authority.Operations.Groups;
+using Xunit;
+
+namespace Authority.IntegrationTests.Groups
+{
+    public sealed class RemovePolicyTests
+    {
+        [Fact]
+        public async Task RemovePolicyFromGroupShouldSucceed()
+        {
+            using (AuthorityTestContext testContext = new AuthorityTestContext())
+            {
+                // Arrange
+                Group group = await TestOperations.CreateGroup(testContext.Context, testContext.Domain.Id, RandomData.RandomString());
+                Policy policy = await TestOperations.CreatePolicy(testContext.Context, testContext.Domain.Id, RandomData.RandomString());
+
+                AddPolicyToGroup addToGroup = new AddPolicyToGroup(testContext.Context, group.Id, policy.Id);
+                await addToGroup.Do();
+                await addToGroup.CommitAsync();
+
+                // Act
+                RemovePolicyFromGroup removeFromGroup = new RemovePolicyFromGroup(testContext.Context, group.Id, policy.Id);
+                await removeFromGroup.Do();
+                await removeFromGroup.CommitAsync();
+
+                // Assert
+                Guid groupId = group.Id;
+                Guid policyId = policy.Id;
+
+                group = await testContext.Context.Groups
+                    .Include(g => g.Policies)
+                    .FirstOrDefaultAsync(g => g.Id == groupId);
+
+                policy = await testContext.Context.Policies
+                    .Include(p => p.Groups)
+                    .FirstOrDefaultAsync(p => p.Id == policyId);
+
+                Assert.NotNull(group);
+                Assert.NotNull(policy);
+
+                Assert.Equal(0, group.Policies.Count);
+                Assert.Equal(0, policy.Groups.Count);
+            }
+        }
+    }
+}
